f6febb7ff38d1aa12d9123aa9f3ea309a18813c9
Shion <shionchill@Shions-MacBook-Pro.local>
2019-10-26 18:50:58 +0700

    Updates

---
+++ precache-manifest.3bc821eb68497baf76c4fe48ee5616fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d2c3db12437d6f2542c9a5f65c7243",
+    "url": "/cat-friends/index.html"
+  },
+  {
+    "revision": "76873864c4d707bf7c31",
+    "url": "/cat-friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "41ddf2675b5b1cde7aaf",
+    "url": "/cat-friends/static/css/main.86a81645.chunk.css"
+  },
+  {
+    "revision": "76873864c4d707bf7c31",
+    "url": "/cat-friends/static/js/2.d005b202.chunk.js"
+  },
+  {
+    "revision": "41ddf2675b5b1cde7aaf",
+    "url": "/cat-friends/static/js/main.ae393e65.chunk.js"
+  },
+  {
+    "revision": "763646bbf72daa664d10",
+    "url": "/cat-friends/static/js/runtime-main.c50bb959.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/cat-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
