a945cb20bf0c1b3780f61938d1609d66a5d49d8d
Shion <shionchill@Shions-MacBook-Pro.local>
2019-10-28 13:42:25 +0700

    Update Service Worker

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ import App from "./containers/App";
 //-- Styles --//
 import "./index.css";
 
+//-- Service Worker --//
+import * as serviceWorker from './serviceWorker';
+
 //----------------------------------------------------//
 //----------------------------------------------------//
 
@@ -34,3 +37,8 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
